c1ea7103b2669c72d3205a9f8aa7612a0dd77113
mhm930 <maham.khan20@ymail.com>
2020-03-22 14:35:52 +0000

    Changing Predict.py to include LS detection

--- Code_Python3/TrackNet_One_Frame_Input/predict_video.py
+++ Code_Python3/TrackNet_One_Frame_Input/predict_video.py
@@ -4,6 +4,81 @@ import queue as Queue
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw
+import time
+from matplotlib import pyplot as plt
+
+def Utils_indexes(y, thres=0.3, min_dist=1):
+    """Peak detection routine.
+
+    Finds the numeric index of the peaks in *y* by taking its first order difference. By using
+    *thres* and *min_dist* parameters, it is possible to reduce the number of
+    detected peaks. *y* must be signed.
+
+    Parameters
+    ----------
+    y : ndarray (signed)
+        1D amplitude data to search for peaks.
+    thres : float between [0., 1.]
+        Normalized threshold. Only the peaks with amplitude higher than the
+        threshold will be detected.
+    min_dist : int
+        Minimum distance between each detected peak. The peak with the highest
+        amplitude is preferred to satisfy this constraint.
+
+    Returns
+    -------
+    ndarray
+        Array containing the numeric indexes of the peaks that were detected
+    """
+    if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.unsignedinteger):
+        raise ValueError("y must be signed")
+
+    thres = thres * (np.max(y) - np.min(y)) + np.min(y)
+    min_dist = int(min_dist)
+
+    # compute first order difference
+    dy = np.diff(y)
+
+    # propagate left and right values successively to fill all plateau pixels (0-value)
+    zeros,=np.where(dy == 0)
+    
+    # check if the singal is totally flat
+    if len(zeros) == len(y) - 1:
+        return np.array([])
+    
+    while len(zeros):
+        # add pixels 2 by 2 to propagate left and right value onto the zero-value pixel
+        zerosr = np.hstack([dy[1:], 0.])
+        zerosl = np.hstack([0., dy[:-1]])
+
+        # replace 0 with right value if non zero
+        dy[zeros]=zerosr[zeros]
+        zeros,=np.where(dy == 0)
+
+        # replace 0 with left value if non zero
+        dy[zeros]=zerosl[zeros]
+        zeros,=np.where(dy == 0)
+
+    # find the peaks by using the first order difference
+    peaks = np.where((np.hstack([dy, 0.]) < 0.)
+                     & (np.hstack([0., dy]) > 0.)
+                     & (y > thres))[0]
+
+    # handle multiple peaks, respecting the minimum distance
+    if peaks.size > 1 and min_dist > 1:
+        highest = peaks[np.argsort(y[peaks])][::-1]
+        rem = np.ones(y.size, dtype=bool)
+        rem[peaks] = False
+
+        for peak in highest:
+            if not rem[peak]:
+                sl = slice(max(0, peak - min_dist), peak + min_dist + 1)
+                rem[sl] = True
+                rem[peak] = False
+
+        peaks = np.arange(y.size)[~rem]
+
+    return peaks
 
 #parse parameters
 parser = argparse.ArgumentParser()
@@ -20,7 +95,7 @@ n_classes =  args.n_classes
 
 if output_video_path == "":
 	#output video in same path
-	output_video_path = '/content/drive/My Drive/' + "_TrackNet_Model1_1000.mp4"
+	output_video_path = '/content/drive/My Drive/' + "_TrackNet_Model1_5695.mp4"
 
 #get video fps&video size
 video = cv2.VideoCapture(input_video_path)
@@ -40,7 +115,7 @@ modelFN = Models.TrackNet.TrackNet
 m = modelFN( n_classes , input_height=height, input_width=width   )
 m.compile(loss='categorical_crossentropy', optimizer= 'adadelta' , metrics=['accuracy'])
 m.load_weights(  save_weights_path  )
-buffer_length = 20
+buffer_length = 10
 # In order to draw the trajectory of tennis, we need to save the coordinate of preious 7 frames 
 q = Queue.deque()
 for i in range(0,buffer_length):
@@ -64,6 +139,10 @@ output_video = cv2.VideoWriter(output_video_path,fourcc, fps, (output_width,outp
 count = 1
 
 while(count < 1000):
+
+	tic_total = time.time()
+	tic = time.time()
+	possible_LS = [];
 
 	#capture frame-by-frame
 	video.set(1,currentFrame); 
@@ -118,9 +197,33 @@ while(count < 1000):
 			x = int(circles[0][0][0])
 			y = int(circles[0][0][1])
 			print (currentFrame, x,y)
-
-			#push x,y to queue
-			q.appendleft([x,y])   
+			temp = []
+			q.appendleft([x,y])          
+			temp = np.array([i for i in q if i]) ;
+			temp.reshape(-1,2)
+			temp_y = temp[:,1]
+			temp_x = temp[:,0]
+			#print ('outliers',detect_outlier(temp_y))
+			indexes = Utils_indexes(temp_y,thres=0.8/max(temp_y), min_dist=0.5)
+			dist = [np.abs(temp_y[i-1]-temp_y[i]) for i in range(1,len(temp_y))]
+			d = (np.std(dist))
+			print(temp_y,dist,d)
+                          
+			if len(indexes)==1:
+			    print ('Possible LS')
+			    print(indexes)
+			    cv2.putText(output_img,'Possible LS',(temp_x[indexes],temp_y[indexes]),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
+			    possible_LS.append([temp_x[indexes],temp_y[indexes]])
+			    image_np1 = output_img.copy()
+                #cv2.putText(image_np,'Possible LS',(uniq_arr[k][0],uniq_arr[k][1]),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
+			    plt.clf()
+			    plt.plot(temp_x,temp_y,linestyle='-', marker='o')
+			    plt.gca().invert_yaxis()
+			    print('indexes',indexes,len(indexes))
+			    print('Difference Index',np.diff(indexes).all())
+			    plt.plot(temp_x[indexes],temp_y[indexes],'.r')
+			    plt.savefig('graph_{}.png'.format(count))
+  
 			#pop x,y from queue
 			q.pop()    
 		else:
@@ -148,7 +251,7 @@ while(count < 1000):
 	opencvImage =  cv2.cvtColor(np.array(PIL_image), cv2.COLOR_RGB2BGR)
 	#write image to output_video
 	output_video.write(opencvImage)
-
+	print("FPS : ", 1 / (time.time() - tic))
 	#next frame
 	currentFrame += 1
 	count += 1
